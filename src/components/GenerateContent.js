de35fc7a8f8a2acbf4e093b7ac7e41e36402947d
CKawara <ndutakawara2@gmail.com>
2023-05-07 16:31:12 +0300

    setup files

---
+++ src/components/GenerateContent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const GenerateContent = () => {
+  return (
+    <div>GenerateContent</div>
+  )
+}
+
+export default GenerateContent
---
+++ src/components/Poster.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import GenerateContent from './GenerateContent'
+
+const Poster = () => {
+  return (
+    <div>
+        <GenerateContent/>
+    </div>
+  )
+}
+
+export default Poster
